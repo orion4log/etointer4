21e20fccfaf6ab55548b04061252cbede10a1171
orion4log <orion4log@yandex.ru>
2026-03-24 20:00:39 +0300

    first commit

---
+++ etointer4.js
@@ -0,0 +1,123 @@
+$(document).ready(function(){ 
+    $(".ubratp1").click(function(){
+        $(".pr1").hide();
+$('.pokazp1').css({'color': 'white'});
+    }); 
+    $(".pokazp1").click(function(){
+        $(".pr1").show();
+$('.pokazp1').css({'color': 'red'});
+    });
+});
+$(document).ready(function(){ 
+    $(".ubratp2").click(function(){
+        $(".pr2").hide();
+        $('.pokazp2').css({'color': 'white'});
+    }); 
+    $(".pokazp2").click(function(){
+        $(".pr2").show();
+        $('.pokazp2').css({'color': 'red'});
+    });
+});
+$(document).ready(function(){ 
+    $(".ubratp3").click(function(){
+        $(".pr3").hide();
+        $('.pokazp3').css({'color': 'white'});
+    }); 
+    $(".pokazp3").click(function(){
+        $(".pr3").show();
+        $('.pokazp3').css({'color': 'red'});
+    });
+});
+$(document).ready(function(){ 
+    $(".ubratp4").click(function(){
+        $(".pr4").hide();
+        $('.pokazp4').css({'color': 'white'});
+    }); 
+    $(".pokazp4").click(function(){
+        $(".pr4").show();
+        $('.pokazp4').css({'color': 'red'});
+    });
+});
+$(document).ready(function(){ 
+    $(".ubratp5").click(function(){
+        $(".pr5").hide();
+        $('.pokazp5').css({'color': 'white'});
+    }); 
+    $(".pokazp5").click(function(){
+        $(".pr5").show();
+        $('.pokazp5').css({'color': 'red'});
+    });
+});
+$(document).ready(function(){ 
+    $(".ubratp6").click(function(){
+        $(".pr6").hide();
+        $('.pokazp6').css({'color': 'white'});
+    }); 
+    $(".pokazp6").click(function(){
+        $(".pr6").show();
+        $('.pokazp6').css({'color': 'red'});
+    });
+});
+
+$(document).ready(function(){ 
+    $(".ubratl1").click(function(){
+        $(".lv1").hide();
+$('.pokazl1').css({'color': 'white'});
+    }); 
+    $(".pokazl1").click(function(){
+        $(".lv1").show();
+$('.pokazl1').css({'color': 'red'});
+    });
+});
+$(document).ready(function(){ 
+    $(".ubratl2").click(function(){
+        $(".lv2").hide();
+        $('.pokazl2').css({'color': 'white'});
+    }); 
+    $(".pokazl2").click(function(){
+        $(".lv2").show();
+        $('.pokazl2').css({'color': 'red'});
+    });
+});
+$(document).ready(function(){ 
+    $(".ubratl3").click(function(){
+        $(".lv3").hide();
+        $('.pokazl3').css({'color': 'white'});
+    }); 
+    $(".pokazl3").click(function(){
+        $(".lv3").show();
+        $('.pokazl3').css({'color': 'red'});
+    });
+});
+$(document).ready(function(){ 
+    $(".ubratl4").click(function(){
+        $(".lv4").hide();
+        $('.pokazl4').css({'color': 'white'});
+    }); 
+    $(".pokazl4").click(function(){
+        $(".lv4").show();
+        $('.pokazl4').css({'color': 'red'});
+    });
+});
+$(document).ready(function(){ 
+    $(".ubratl5").click(function(){
+        $(".lv5").hide();
+        $('.pokazl5').css({'color': 'white'});
+    }); 
+    $(".pokazl5").click(function(){
+        $(".lv5").show();
+        $('.pokazl5').css({'color': 'red'});
+    });
+});
+$(document).ready(function(){ 
+    $(".ubratl6").click(function(){
+        $(".lv6").hide();
+        $('.pokazl6').css({'color': 'white'});
+    }); 
+    $(".pokazl6").click(function(){
+        $(".lv6").show();
+        $('.pokazl6').css({'color': 'red'});
+    });
+});
+
+
